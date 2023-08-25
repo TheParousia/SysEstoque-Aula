1103abcfb3c8515bc38668459adf323f68000afc
Gabriel Pimentel <131470415+TheParousia@users.noreply.github.com>
2023-08-25 12:41:08 -0300

    Add função de criação de chave AES

--- Utils/CryptAES.cs
+++ Utils/CryptAES.cs
@@ -15,6 +15,12 @@ namespace SysEstoque.Utils {
 		private static byte[] IV = { 0x50, 0x08, 0xF1, 0xDD, 0xDE, 0x3C, 0xF2, 0x18, 0x44, 0x74, 0x19, 0x2C, 0x53, 0x49, 0xAB, 0xBC };
 		private static byte[] salt = { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x5, 0x4, 0x3, 0x2, 0x1, 0x0 };
 
+		internal static void makeKey(string _Key) {
+			Rfc2898DeriveBytes chave = new Rfc2898DeriveBytes(_Key, salt);
+
+			Key = chave.GetBytes(16);
+		}
+
 		public static string Encriptar(string plainText) {
 			// Check arguments. 
 			if (plainText == null || plainText.Length <= 0)
@@ -85,70 +91,5 @@ namespace SysEstoque.Utils {
 
 			return plainText;
 		}
-
-
-	//=======================================
-
-
-		static void EncryptAesManaged(string raw) {
-			try {
-				// Create Aes that generates a new key and initialization vector (IV).    
-				// Same key must be used in encryption and decryption    
-				using (AesManaged aes = new AesManaged()) {
-					// Encrypt string    
-					byte[] encrypted = Encrypt(raw, aes.Key, aes.IV);
-					// Print encrypted string    
-					Console.WriteLine($"Encrypted data: {System.Text.Encoding.UTF8.GetString(encrypted)}");
-					// Decrypt the bytes to a string.    
-					string decrypted = Decrypt(encrypted, aes.Key, aes.IV);
-					// Print decrypted string. It should be same as raw data    
-					Console.WriteLine($"Decrypted data: {decrypted}");
-				}
-			} catch (Exception exp) {
-				Console.WriteLine(exp.Message);
-			}
-			Console.ReadKey();
-		}
-		static byte[] Encrypt(string plainText, byte[] Key, byte[] IV) {
-			byte[] encrypted;
-			// Create a new AesManaged.    
-			using (AesManaged aes = new AesManaged()) {
-				// Create encryptor    
-				ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
-				// Create MemoryStream    
-				using (MemoryStream ms = new MemoryStream()) {
-					// Create crypto stream using the CryptoStream class. This class is the key to encryption    
-					// and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
-					// to encrypt    
-					using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write)) {
-						// Create StreamWriter and write data to a stream    
-						using (StreamWriter sw = new StreamWriter(cs))
-							sw.Write(plainText);
-						encrypted = ms.ToArray();
-					}
-				}
-			}
-			// Return encrypted data    
-			return encrypted;
-		}
-		static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV) {
-			string plaintext = null;
-			// Create AesManaged    
-			using (AesManaged aes = new AesManaged()) {
-				// Create a decryptor    
-				ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
-				// Create the streams used for decryption.    
-				using (MemoryStream ms = new MemoryStream(cipherText)) {
-					// Create crypto stream    
-					using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read)) {
-						// Read crypto stream    
-						using (StreamReader reader = new StreamReader(cs))
-							plaintext = reader.ReadToEnd();
-					}
-				}
-			}
-			return plaintext;
-		}
-
 	}
 }
